61cc678ee8700de5aa12830ba05e4e1a15ed13b0
Rayhan-abdullha <programmer.rayhan1@gmail.com>
2025-01-30 23:25:13 +0600

    add blog

--- src/app/blog/[...id]/page.tsx
+++ src/app/blog/[...id]/page.tsx
@@ -1,36 +1,59 @@
 'use client'
 import { Section, useBlogContext } from "@/context/BlogContext";
 import { useParams } from "next/navigation";
-import { Blog } from "../components/BlogForm"; // Import BlogForm type
+import { Blog } from "../components/BlogForm";
 import YouTubePlayer from "@/app/components/YoutubePlayer";
+import Image from "next/image";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const BlogDetail = () => {
   const { getBlogById } = useBlogContext();
   const params = useParams();
   const id = params?.id as string;
 
-  const blog: Blog | undefined = getBlogById(id); // Get blog by ID
+  const blog: Blog | undefined = getBlogById(id);
 
   if (blog === undefined) {
     return <p className="text-center text-lg">There is no blog available.</p>;
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg">
-      {/* Blog Title */}
-      <h1 className="text-3xl font-bold mb-6">{blog.title}</h1>
-
-      {/* Blog Cover Image */}
-      {blog.coverImage && (
-        <div className="mb-6">
-          <img
-            src={blog.coverImage}
-            alt="Cover Image"
-            className="w-full h-auto rounded-lg"
-          />
+    <div>
+      <div className="bg-gray-200 dark:bg-gray-800 py-10 md:py-14 overflow-hidden mt-[-26px]">
+        <div className="max-w-4xl mx-auto items-center flex flex-col lg:flex-row gap-10 ">
+          <div>
+            <h5 className="text-xl font-bold mb-6">Javascript</h5>
+            <h1 className="text-3xl font-bold mb-6">{blog.title}</h1>
+            <div className="flex items-center gap-2">
+              <Image
+                src="https://th.bing.com/th/id/OIP.wEsBe2udHBieFeZVmus8qAHaHk?rs=1&pid=ImgDetMain"
+                alt="not found"
+                width={50}
+                height={50}
+                className="w-[50px] h-[50px] rounded-full object-cover"
+              />
+              <div className="flex flex-col gap-1 sm:flex-row">
+                <p>Rayhan Abdullah</p>
+                <p className="text-gray-600 dark:text-gray-400">1 hour ago</p>
+              </div>
+            </div>
+          </div>
+            {/* Blog Cover Image */}
+            {blog.coverImage && (
+            <div className="mb-6 lg:mb-0">
+              <Image
+              src={blog.coverImage}
+              alt="Cover Image"
+                className="px-5 md:p-0 md:w-[400px] h-auto rounded-lg"
+                width={500}
+                height={500}
+            />
+          </div>
+          )}
         </div>
-      )}
-
+      </div>
+      <div className="max-w-4xl mx-auto p-6 shadow-lg rounded-lg">
       {/* Dynamic Sections */}
       {blog.sections.map((section: Section, index: number) => (
         <div key={index} className="mb-6">
@@ -43,11 +66,17 @@ const BlogDetail = () => {
           {section.type === "paragraph" && (
             <p className="text-gray-700 mb-4">{section.content}</p>
           )}
-          {section.type === "code" && (
-            <pre className="bg-gray-100 p-4 rounded-lg overflow-x-auto">
-              <code className="text-sm">{section.content}</code>
-            </pre>
-          )}
+        {section.type === "code" && (
+            <SyntaxHighlighter
+              language="javascript" 
+              style={oneDark} 
+              wrapLines
+              customStyle={{ fontSize: "0.875rem" }}
+            >
+              {section.content}
+            </SyntaxHighlighter>
+     
+        )}
           {section.type === "image" && (
             <div className="mb-4">
               <img
@@ -64,6 +93,7 @@ const BlogDetail = () => {
           )}
         </div>
       ))}
+      </div>
     </div>
   );
 };
